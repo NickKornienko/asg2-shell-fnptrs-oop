31847ad4e978b14698b6875c12be1f84fbf17677
Nick Kornienko <nkornien@ucsc.edu>
2022-01-29 17:55:15 -0800

    pwd

--- code/file_sys.cpp
+++ code/file_sys.cpp
@@ -1,4 +1,4 @@
-// $Id: file_sys.cpp,v 1.11 2022-01-14 18:48:50-08 - - $
+// $Id: file_sys.cpp,v 1.13 2022-01-26 16:10:48-08 - - $
 
 #include <cassert>
 #include <iostream>
@@ -9,119 +9,160 @@ using namespace std;
 #include "debug.h"
 #include "file_sys.h"
 
-size_t inode::next_inode_nr {1};
+size_t inode::next_inode_nr{1};
 
 inode_ptr inode_state::get_cwd()
 {
    return cwd;
 }
 
-void inode_state::set_cwd()
+base_file_ptr inode::get_contents()
 {
+   return contents;
 }
 
-ostream& operator<< (ostream& out, file_type type) {
-   switch (type) {
-      case file_type::PLAIN_TYPE: out << "PLAIN_TYPE"; break;
-      case file_type::DIRECTORY_TYPE: out << "DIRECTORY_TYPE"; break;
-      default: assert (false);
+ostream &operator<<(ostream &out, file_type type)
+{
+   switch (type)
+   {
+   case file_type::PLAIN_TYPE:
+      out << "PLAIN_TYPE";
+      break;
+   case file_type::DIRECTORY_TYPE:
+      out << "DIRECTORY_TYPE";
+      break;
+   default:
+      assert(false);
    };
    return out;
 }
 
-inode_state::inode_state() {
-   DEBUGF ('i', "root = " << root << ", cwd = " << cwd
-          << ", prompt = \"" << prompt() << "\"");
+inode_state::inode_state()
+{
+   root = cwd = make_shared<inode>(file_type::DIRECTORY_TYPE);
+   directory_entries &dirents = root->get_dirents();
+   dirents.insert(dirent_type(".", root));
+   dirents.insert(dirent_type("..", root));
 }
 
-const string& inode_state::prompt() const { return prompt_; }
+const string &inode_state::prompt() const { return prompt_; }
 
-void inode_state::prompt (const string& new_prompt) {
+void inode_state::prompt(const string &new_prompt)
+{
    prompt_ = new_prompt;
 }
 
-ostream& operator<< (ostream& out, const inode_state& state) {
+ostream &operator<<(ostream &out, const inode_state &state)
+{
    out << "inode_state: root = " << state.root
        << ", cwd = " << state.cwd;
    return out;
 }
 
-inode::inode(file_type type): inode_nr (next_inode_nr++) {
-   switch (type) {
-      case file_type::PLAIN_TYPE:
-           contents = make_shared<plain_file>();
-           break;
-      case file_type::DIRECTORY_TYPE:
-           contents = make_shared<directory>();
-           break;
-      default: assert (false);
+inode::inode(file_type type) : inode_nr(next_inode_nr++)
+{
+   switch (type)
+   {
+   case file_type::PLAIN_TYPE:
+      contents = make_shared<plain_file>();
+      break;
+   case file_type::DIRECTORY_TYPE:
+      contents = make_shared<directory>();
+      break;
+   default:
+      assert(false);
    }
-   DEBUGF ('i', "inode " << inode_nr << ", type = " << type);
+   DEBUGF('i', "inode " << inode_nr << ", type = " << type);
 }
 
-size_t inode::get_inode_nr() const {
-   DEBUGF ('i', "inode = " << inode_nr);
+size_t inode::get_inode_nr() const
+{
+   DEBUGF('i', "inode = " << inode_nr);
    return inode_nr;
 }
 
-
-file_error::file_error (const string& what):
-            runtime_error (what) {
+directory_entries &inode::get_dirents()
+{
+   return contents->get_dirents();
 }
 
-const wordvec& base_file::readfile() const {
-   throw file_error ("is a " + error_file_type());
+file_error::file_error(const string &what) : runtime_error(what)
+{
 }
 
-void base_file::writefile (const wordvec&) {
-   throw file_error ("is a " + error_file_type());
+const wordvec &base_file::readfile() const
+{
+   throw file_error("is a " + file_type());
 }
 
-void base_file::remove (const string&) {
-   throw file_error ("is a " + error_file_type());
+void base_file::writefile(const wordvec &)
+{
+   throw file_error("is a " + file_type());
 }
 
-inode_ptr base_file::mkdir (const string&) {
-   throw file_error ("is a " + error_file_type());
+void base_file::remove(const string &)
+{
+   throw file_error("is a " + file_type());
 }
 
-inode_ptr base_file::mkfile (const string&) {
-   throw file_error ("is a " + error_file_type());
+inode_ptr base_file::mkdir(const string &)
+{
+   throw file_error("is a " + file_type());
 }
 
-
-size_t plain_file::size() const {
-   size_t size {0};
-   DEBUGF ('i', "size = " << size);
+inode_ptr base_file::mkfile(const string &)
+{
+   throw file_error("is a " + file_type());
+}
+
+directory_entries &base_file::get_dirents()
+{
+   throw file_error("is a " + file_type());
+}
+
+size_t plain_file::size() const
+{
+   size_t size{0};
+   DEBUGF('i', "size = " << size);
    return size;
 }
 
-const wordvec& plain_file::readfile() const {
-   DEBUGF ('i', data);
+const wordvec &plain_file::readfile() const
+{
+   DEBUGF('i', data);
    return data;
 }
 
-void plain_file::writefile (const wordvec& words) {
-   DEBUGF ('i', words);
+void plain_file::writefile(const wordvec &words)
+{
+   DEBUGF('i', words);
 }
 
-size_t directory::size() const {
-   size_t size {0};
-   DEBUGF ('i', "size = " << size);
+size_t directory::size() const
+{
+   size_t size{0};
+   DEBUGF('i', "size = " << size);
    return size;
 }
 
-void directory::remove (const string& filename) {
-   DEBUGF ('i', filename);
+void directory::remove(const string &filename)
+{
+   DEBUGF('i', filename);
 }
 
-inode_ptr directory::mkdir (const string& dirname) {
-   DEBUGF ('i', dirname);
+inode_ptr directory::mkdir(const string &dirname)
+{
+   DEBUGF('i', dirname);
    return nullptr;
 }
 
-inode_ptr directory::mkfile (const string& filename) {
-   DEBUGF ('i', filename);
+inode_ptr directory::mkfile(const string &filename)
+{
+   DEBUGF('i', filename);
    return nullptr;
 }
 
+directory_entries &directory::get_dirents()
+{
+   return dirents;
+}
--- code/file_sys.h
+++ code/file_sys.h
@@ -1,4 +1,4 @@
-// $Id: file_sys.h,v 1.11 2021-12-20 12:55:34-08 - - $
+// $Id: file_sys.h,v 1.13 2022-01-26 16:10:48-08 - - $
 
 #ifndef INODE_H
 #define INODE_H
@@ -15,36 +15,44 @@ using namespace std;
 // inode_t -
 //    An inode is either a directory or a plain file.
 
-enum class file_type {PLAIN_TYPE, DIRECTORY_TYPE};
+enum class file_type
+{
+   PLAIN_TYPE,
+   DIRECTORY_TYPE
+};
 class inode;
 class base_file;
 class plain_file;
 class directory;
 using inode_ptr = shared_ptr<inode>;
 using base_file_ptr = shared_ptr<base_file>;
-ostream& operator<< (ostream&, file_type);
+using directory_entries = map<string, inode_ptr>;
+using dirent_type = directory_entries::value_type;
+ostream &operator<<(ostream &, file_type);
 
-
 // inode_state -
 //    A small convenient class to maintain the state of the simulated
 //    process:  the root (/), the current directory (.), and the
 //    prompt.
 
-class inode_state {
+class inode_state
+{
    friend class inode;
-   friend ostream& operator<< (ostream& out, const inode_state&);
-   private:
-      inode_ptr root {nullptr};
-      inode_ptr cwd {nullptr};
-      string prompt_ {"% "};
-   public:
-      inode_ptr get_cwd();
-      void set_cwd();
-      inode_state (const inode_state&) = delete; // copy ctor
-      inode_state& operator= (const inode_state&) = delete; // op=
-      inode_state();
-      const string& prompt() const;
-      void prompt (const string&);
+   friend ostream &operator<<(ostream &out, const inode_state &);
+
+private:
+   inode_ptr root{nullptr};
+   inode_ptr cwd{nullptr};
+   string prompt_{"% "};
+
+public:
+   inode_ptr get_cwd();
+   inode_state(const inode_state &) = delete;            // copy ctor
+   inode_state &operator=(const inode_state &) = delete; // op=
+   inode_state();
+   const string &prompt() const;
+   void prompt(const string &);
+   const inode_ptr get_root() const { return root; }
 };
 
 // class inode -
@@ -58,49 +66,60 @@ class inode_state {
 //    number of dirents.  For a text file, the number of characters
 //    when printed (the sum of the lengths of each word, plus the
 //    number of words.
-//    
+//
 
-class inode {
+class inode
+{
    friend class inode_state;
-   private:
-      static size_t next_inode_nr;
-      size_t inode_nr;
-      base_file_ptr contents;
-   public:
-      inode() = delete;
-      inode (const inode&) = delete;
-      inode& operator= (const inode&) = delete;
-      inode (file_type);
-      size_t get_inode_nr() const;
+
+private:
+   static size_t next_inode_nr;
+   size_t inode_nr;
+   base_file_ptr contents;
+
+public:
+   base_file_ptr get_contents();
+   inode() = delete;
+   inode(const inode &) = delete;
+   inode &operator=(const inode &) = delete;
+   inode(file_type);
+   size_t get_inode_nr() const;
+   directory_entries &get_dirents();
 };
 
-
 // class base_file -
 // Just a base class at which an inode can point.  No data or
 // functions.  Makes the synthesized members useable only from
 // the derived classes.
 
-class file_error: public runtime_error {
-   public:
-      explicit file_error (const string& what);
+class file_error : public runtime_error
+{
+public:
+   explicit file_error(const string &what);
 };
 
-class base_file {
-   protected:
-      base_file() = default;
-      virtual const string& error_file_type() const = 0;
-   public:
-      virtual ~base_file() = default;
-      base_file (const base_file&) = delete;
-      base_file& operator= (const base_file&) = delete;
-      virtual size_t size() const = 0;
-      virtual const wordvec& readfile() const;
-      virtual void writefile (const wordvec& newdata);
-      virtual void remove (const string& filename);
-      virtual inode_ptr mkdir (const string& dirname);
-      virtual inode_ptr mkfile (const string& filename);
+class base_file
+{
+   friend class inode_state;
+   friend class inode;
+
+protected:
+   base_file() = default;
+   virtual const string &file_type() const = 0;
+
+public:
+   virtual ~base_file() = default;
+   base_file(const base_file &) = delete;
+   base_file &operator=(const base_file &) = delete;
+   virtual size_t size() const = 0;
+   virtual const wordvec &readfile() const;
+   virtual void writefile(const wordvec &newdata);
+   virtual void remove(const string &filename);
+   virtual inode_ptr mkdir(const string &dirname);
+   virtual inode_ptr mkfile(const string &filename);
+   virtual directory_entries &get_dirents();
 };
-
+
 // class plain_file -
 // Used to hold data.
 // synthesized default ctor -
@@ -110,17 +129,20 @@ class base_file {
 // writefile -
 //    Replaces the contents of a file with new contents.
 
-class plain_file: public base_file {
-   private:
-      wordvec data;
-      virtual const string& error_file_type() const override {
-         static const string result = "plain file";
-         return result;
-      }
-   public:
-      virtual size_t size() const override;
-      virtual const wordvec& readfile() const override;
-      virtual void writefile (const wordvec& newdata) override;
+class plain_file : public base_file
+{
+private:
+   wordvec data;
+   virtual const string &file_type() const override
+   {
+      static const string result = "plain file";
+      return result;
+   }
+
+public:
+   virtual size_t size() const override;
+   virtual const wordvec &readfile() const override;
+   virtual void writefile(const wordvec &newdata) override;
 };
 
 // class directory -
@@ -133,7 +155,7 @@ class plain_file: public base_file {
 //    does not exist, or the subdirectory is not empty.
 //    Here empty means the only entries are dot (.) and dotdot (..).
 // mkdir -
-//    Creates a new directory under the current directory and 
+//    Creates a new directory under the current directory and
 //    immediately adds the directories dot (.) and dotdot (..) to it.
 //    Note that the parent (..) of / is / itself.  It is an error
 //    if the entry already exists.
@@ -141,20 +163,23 @@ class plain_file: public base_file {
 //    Create a new empty text file with the given name.  Error if
 //    a dirent with that name exists.
 
-class directory: public base_file {
-   private:
-      // Must be a map, not unordered_map, so printing is lexicographic
-      map<string,inode_ptr> dirents;
-      virtual const string& error_file_type() const override {
-         static const string result = "directory";
-         return result;
-      }
-   public:
-      virtual size_t size() const override;
-      virtual void remove (const string& filename) override;
-      virtual inode_ptr mkdir (const string& dirname) override;
-      virtual inode_ptr mkfile (const string& filename) override;
+class directory : public base_file
+{
+private:
+   // Must be a map, not unordered_map, so printing is lexicographic
+   directory_entries dirents;
+   virtual const string &file_type() const override
+   {
+      static const string result = "directory";
+      return result;
+   }
+
+public:
+   virtual size_t size() const override;
+   virtual void remove(const string &filename) override;
+   virtual inode_ptr mkdir(const string &dirname) override;
+   virtual inode_ptr mkfile(const string &filename) override;
+   virtual directory_entries &get_dirents() override;
 };
 
 #endif
-
